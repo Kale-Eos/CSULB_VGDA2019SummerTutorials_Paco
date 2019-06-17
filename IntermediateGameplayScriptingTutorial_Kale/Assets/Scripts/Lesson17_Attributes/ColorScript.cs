e2143293833c1cc5972d248a80d15134543e0417
Kale-Eos <pacoellaga@gmail.com>
2019-06-16 20:16:14 -0700

    finishe lesson 17 and demo - attributes (made bonus custom edits)

---
+++ IntermediateGameplayScriptingTutorial_Kale/Assets/Scripts/Lesson17_Attributes/ColorScript.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]   // creates perma changes
+
+public class ColorScript : MonoBehaviour
+{
+    void Start()
+    {
+        //renderer.sharedMaterial.color = Color.red;
+    }
+}
---
+++ IntermediateGameplayScriptingTutorial_Kale/Assets/Scripts/Lesson17_Attributes/SpinScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// note only Yspeed was used in tutorial
+// made edits because I wanted to
+
+[ExecuteInEditMode]
+public class SpinScript : MonoBehaviour
+{
+    [Range(-100, 100)] public int Xspeed = 0;    // creates sliders in edit
+    [Range(-100, 100)] public int Yspeed = 0;    // creates sliders in edit
+    [Range(-100, 100)] public int Zspeed = 0;    // creates sliders in edit
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(Xspeed * Time.deltaTime, 0, 0));
+        transform.Rotate(new Vector3(0, Yspeed * Time.deltaTime, 0));
+        transform.Rotate(new Vector3(0, 0, Zspeed * Time.deltaTime));
+    }
+}
