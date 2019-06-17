f7cf646da603a47b585c1b14532b2acc1144e62c
Kale-Eos <pacoellaga@gmail.com>
2019-06-16 20:02:23 -0700

    finished 17 and demo (delagates)

---
+++ IntermediateGameplayScriptingTutorial_Kale/Assets/Scripts/Lesson16_Delegates/DelegateScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateScript : MonoBehaviour
+{
+    // variables contain data
+    // delegates contain functions
+
+    delegate void MyDelegate(int num);  // delegate parameters
+    MyDelegate myDelegate;              // member variable
+
+    void Start()
+    {
+        myDelegate = PrintNum;          // sets number
+        myDelegate(50);                 // sets 50
+
+        myDelegate = DoubleNum;         // function to param
+        myDelegate(50);                 // increment amount
+    }
+
+    void PrintNum(int num)
+    {
+        print("Print Num: " + num);
+    }
+
+    void DoubleNum(int num)
+    {
+        print("Double Num: " + num * 2);
+    }
+}
---
+++ IntermediateGameplayScriptingTutorial_Kale/Assets/Scripts/Lesson16_Delegates/MultiCastScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiCastScript : MonoBehaviour
+{
+    delegate void MultiDelegate();
+    MultiDelegate myMultiDelegate;
+
+    void Start()
+    {
+        myMultiDelegate += PowerUp;
+        myMultiDelegate += TurnGreen;
+
+        if (myMultiDelegate != null)
+        {
+            myMultiDelegate();
+        }
+    }
+
+    void PowerUp()
+    {
+        print("Orb is powering up!");
+    }
+
+    // original used from tutorial
+    //void TurnGreen()
+    //{
+    //    GetComponent<Renderer>().material.color = Color.green;
+    //}
+
+    //added following part to see function delayed
+    void TurnGreen()
+    {
+        StartCoroutine(TurningGreen());
+    }
+
+    IEnumerator TurningGreen()
+    {
+        yield return new WaitForSeconds(2f);
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+}
