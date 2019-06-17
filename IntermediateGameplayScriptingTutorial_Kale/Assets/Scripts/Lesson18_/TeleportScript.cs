fdf53d1d7fb0b420886fe11d0d8c2ed8c1454b64
Kale-Eos <pacoellaga@gmail.com>
2019-06-16 20:37:43 -0700

    final lesson complete

---
+++ IntermediateGameplayScriptingTutorial_Kale/Assets/Scripts/Lesson18_/TeleportScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+    void OnEnable()
+    {
+        EventManager.OnClicked += Teleport; // subscribe to method
+    }
+
+    void OnDisable()
+    {
+        EventManager.OnClicked -= Teleport; // unsubscribed to method
+    }
+
+    void Teleport()
+    {
+        Vector3 pos = transform.position;
+        pos.y = Random.Range(1.0f, 3.0f);
+        transform.position = pos;
+    }
+}
